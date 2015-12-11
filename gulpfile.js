c1942a3a8cd3e45fca509965541ff05016f2cc64
Zia Khan <ziaukhan@hotmail.com>
2015-12-11 06:25:31 +0500

    removed bug of dot files not being copied

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp-param')(require('gulp'), process.argv);//https://www.npmjs.com/package/gulp-param
 var preprocess = require('gulp-preprocess');//https://www.npmjs.com/package/gulp-preprocess
 
+//complete docs https://github.com/jsoverson/preprocess#directive-syntax
+
 var configFileName = "appConfig.json";//default name
 
 
 gulp.task('gen', function(config) {
   configFileName = config || configFileName;
   var config = require('./' + configFileName);
-  gulp.src(['./src/**/*.*', '!./src/node_modules/**/*.*', '!./src/public/libs/**/*.*'])
+  gulp.src(['./src/**/*.*', '!./src/node_modules/**/*.*', '!./src/public/libs/**/*.*'], {dot: true})
     .pipe(preprocess({context: config})) //To set environment variables in-line 
     .pipe(gulp.dest('./' + config.dirName));
     
